6d1b600e8a15fc2cf6d167bfcbb2a341a5b99ab4
David Chambers <David.Chambers.05@gmail.com>
2010-12-17 01:09:10 +1100

    Reduced indentation to two spaces.

--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,53 @@
 jQuery(function ($) {
 
-    // simulate `:hover`
-    (function () {
-        if (navigator.userAgent.match(/(iPhone|iPod|iPad)/i)) {
-            var $a = $('a').live('touchstart', function () { $(this).addClass('hover'); }).live('touchend', function () { $a.removeClass('hover'); });
+  // simulate `:hover`
+  (function () {
+    if (navigator.userAgent.match(/(iPhone|iPod|iPad)/i)) {
+      var $a = $('a').live('touchstart', function () { $(this).addClass('hover'); }).live('touchend', function () { $a.removeClass('hover'); });
+    }
+  }());
+
+  // accommodate fixed-position header
+  (function (id) {
+    $.each($('h2[id],h3[id],h4[id]'), function () {
+      (id = this.id) && $(this).attr('id', null).addClass(id+' unidentified').prepend('<span id='+id+'>').append('<a href=#'+id+'>\u00B6</a>');
+    });
+  }());
+
+  // scrolling
+  (function () {
+    $('a[href^="#"]').click(function (event) {
+      event.preventDefault();
+      var
+        delta, intervalId, n, offset,
+        positions, round = Math.round,
+        hash = $(this).attr('href'),
+        $el = $(hash);
+
+      if ($el.length != 1) return;
+
+      offset = $el.offset().top;
+      delta = offset - window.pageYOffset;
+      n = round(Math.log(Math.abs(delta)) * 2) || 1;
+
+      $.each(positions = new Array(n), function (index) {
+        positions[index] = round(offset - index*(delta/n));
+      });
+
+      intervalId = window.setInterval(function () {
+        var position = positions.pop();
+        if (typeof position != 'undefined') {
+          window.scrollTo(0, position);
+        } else {
+          window.location.hash = hash;
+          window.clearInterval(intervalId);
         }
-    }());
+      }, 20);
+    });
+  }());
 
-    // accommodate fixed-position header
-    (function (id) {
-        $.each($('h2[id],h3[id],h4[id]'), function () {
-            (id = this.id) && $(this).attr('id', null).addClass(id+' unidentified').prepend('<span id='+id+'>').append('<a href=#'+id+'>\u00B6</a>');
-        });
-    }());
-
-    // scrolling
-    (function () {
-        $('a[href^="#"]').click(function (event) {
-            event.preventDefault();
-            var delta, intervalId, n, offset,
-                positions, round = Math.round,
-                hash = $(this).attr('href'),
-                $el = $(hash);
-
-            if ($el.length != 1) return;
-
-            offset = $el.offset().top;
-            delta = offset - window.pageYOffset;
-            n = round(Math.log(Math.abs(delta)) * 2) || 1;
-
-            $.each(positions = new Array(n), function (index) {
-                positions[index] = round(offset - index*(delta/n));
-            });
-
-            intervalId = window.setInterval(function () {
-                var position = positions.pop();
-                if (typeof position != 'undefined') {
-                    window.scrollTo(0, position);
-                } else {
-                    window.location.hash = hash;
-                    window.clearInterval(intervalId);
-                }
-            }, 20);
-        });
-    }());
-
-    // syntax highlighting
-    $('pre>code').addClass('prettyprint');
-    prettyPrint();
+  // syntax highlighting
+  $('pre>code').addClass('prettyprint');
+  prettyPrint();
 
 });
