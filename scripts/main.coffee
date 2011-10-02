9f378bd742d3345964613fc6b63694ee15c01ac4
David Chambers <David.Chambers.05@gmail.com>
2011-10-02 05:57:29 -0700

    Converted JavaScript to CoffeeScript.

---
+++ scripts/main.coffee
@@ -0,0 +1,66 @@
+jQuery ($) ->
+
+  {location, setInterval, clearInterval} = window
+  {get, trim} = $
+
+  # simulate `:hover`
+  if /(iPhone|iPod|iPad)/i.test navigator.userAgent
+    $(document.body).delegate 'a',
+      touchstart: -> $(this).addClass 'hover'
+      touchend: -> $(this).removeClass 'hover'
+
+  # accommodate fixed-position header
+  $('h2[id],h3[id],h4[id]').each ->
+    {id} = this
+    if id
+      $(this)
+        .addClass("#{id} unidentified")
+        .prepend("<span id='#{id}'>")
+        .append("<a href='##{id}'>¶</a>")
+        .attr('id', null)
+
+  # scrolling
+  $('a[href^="#"]').click (event) ->
+    hash = $(this).attr 'href'
+    if $(hash).length is 1
+      {abs, log, round} = Math
+      offset = $(hash).offset().top
+      delta = offset - window.pageYOffset
+      steps = round(2 * log abs delta) || 1
+      positions = (round offset - idx * (delta / steps) for idx in [0..steps])
+      intervalId = setInterval ->
+        position = positions.pop()
+        if position?
+          window.scrollTo 0, position
+        else
+          location.hash = hash
+          clearInterval intervalId
+      , 20
+      event.preventDefault()
+
+  # syntax highlighting
+  $('pre').children('code').each ->
+    $code = $ this
+    text = trim $code.text()
+    if /^#!\/usr\/bin\/osascript\s/.test text
+      $code.parent().addClass('brush:applescript').text text
+    else
+      $code.addClass 'prettyprint'
+
+  do prettyPrint
+
+  # date and time localization
+  $time = $ 'time'
+  $comments = $time.filter ':has(span)'
+  $time.not($comments).localize()
+  $comments.localize
+    escaped: yes
+    format: '<span>%d %mmmm %yyyy</span> <span>%h.%MM\u2009%a</span>'
+    periods: ['am', 'pm']
+
+  # reload link
+  $('#nav').find('a[href="/flushcache/"]').click (event) ->
+    get @href, -> location.reload yes
+    event.preventDefault()
+
+  $('textarea').each -> Hashify.editor this
--- scripts/main.js
+++ scripts/main.js
@@ -1,131 +1,79 @@
-jQuery(function ($) {
-
-  // simulate `:hover`
+jQuery(function($) {
+  var $comments, $time, clearInterval, get, location, setInterval, trim;
+  location = window.location, setInterval = window.setInterval, clearInterval = window.clearInterval;
+  get = $.get, trim = $.trim;
   if (/(iPhone|iPod|iPad)/i.test(navigator.userAgent)) {
-    $('a')
-      .live(
-        'touchstart',
-        function () {
-          $(this).addClass('hover');
-        }
-      )
-      .live(
-        'touchend',
-        function () {
-          $(this).removeClass('hover');
-        }
-      );
-  }
-
-  // accommodate fixed-position header
-  $('h2[id],h3[id],h4[id]')
-    .each(
-      function () {
-        var id = this.id;
-        if (id) {
-          $(this)
-            .attr('id', null)
-            .addClass(id+' unidentified')
-            .prepend('<span id='+id+'>')
-            .append('<a href=#'+id+'>¶</a>');
-        }
+    $(document.body).delegate('a', {
+      touchstart: function() {
+        return $(this).addClass('hover');
+      },
+      touchend: function() {
+        return $(this).removeClass('hover');
       }
-    );
-
-  // scrolling
-  (function (undef) {
-    $('a[href^="#"]')
-      .click(
-        function (event) {
-          var
-            delta, intervalId, n, offset,
-            positions, round = Math.round,
-            hash = $(this).attr('href'),
-            $el = $(hash);
-
-          if ($el.length === 1) {
-            offset = $el.offset().top;
-            delta = offset - window.pageYOffset;
-            n = round(Math.log(Math.abs(delta)) * 2) || 1;
-
-            $.each(
-              positions = new Array(n),
-              function (index) {
-                positions[index] = round(offset - index*(delta/n));
-              }
-            );
-
-            intervalId =
-              window.setInterval(
-                function () {
-                  var position = positions.pop();
-                  if (position !== undef) {
-                    window.scrollTo(0, position);
-                  } else {
-                    window.location.hash = hash;
-                    window.clearInterval(intervalId);
-                  }
-                },
-                20
-              );
-            event.preventDefault();
-          }
-        }
-      );
-  }());
-
-  // syntax highlighting
-  $('pre>code')
-    .each(
-      function () {
-        var
-          $this = $(this),
-          text = $.trim($this.text());
-
-        if (/^#!\/usr\/bin\/osascript\s/.test(text)) {
-          $this
-            .parent()
-              .addClass('brush:applescript')
-              .text(text);
-        } else {
-          $this
-            .addClass('prettyprint');
-        }
-      }
-    );
-  prettyPrint();
-
-  // date and time localization
-  (function () {
-    var $comments = $('time>span+span').parent();
-    $('time').not($comments).localize();
-    $comments.localize({
-      escaped: true,
-      format: '<span>%d %mmmm %yyyy</span> <span>%h.%MM\u2009%a</span>',
-      periods: ['am', 'pm']
     });
-  }());
-
-  // reload link
-  $('#nav')
-    .find('a[href="/flushcache/"]')
-      .click(
-        function (event) {
-          $.get(
-            this.href,
-            function () {
-              window.location.reload(true);
-            }
-          );
-          event.preventDefault();
+  }
+  $('h2[id],h3[id],h4[id]').each(function() {
+    var id;
+    id = this.id;
+    if (id) {
+      return $(this).addClass("" + id + " unidentified").prepend("<span id='" + id + "'>").append("<a href='#" + id + "'>¶</a>").attr('id', null);
+    }
+  });
+  $('a[href^="#"]').click(function(event) {
+    var abs, delta, hash, idx, intervalId, log, offset, positions, round, steps;
+    hash = $(this).attr('href');
+    if ($(hash).length === 1) {
+      abs = Math.abs, log = Math.log, round = Math.round;
+      offset = $(hash).offset().top;
+      delta = offset - window.pageYOffset;
+      steps = round(2 * log(abs(delta))) || 1;
+      positions = (function() {
+        var _results;
+        _results = [];
+        for (idx = 0; 0 <= steps ? idx <= steps : idx >= steps; 0 <= steps ? idx++ : idx--) {
+          _results.push(round(offset - idx * (delta / steps)));
         }
-      );
-
-  $('textarea')
-    .each(
-      function () {
-        Hashify.editor(this);
-      }
-    );
-
+        return _results;
+      })();
+      intervalId = setInterval(function() {
+        var position;
+        position = positions.pop();
+        if (position != null) {
+          return window.scrollTo(0, position);
+        } else {
+          location.hash = hash;
+          return clearInterval(intervalId);
+        }
+      }, 20);
+      return event.preventDefault();
+    }
+  });
+  $('pre').children('code').each(function() {
+    var $code, text;
+    $code = $(this);
+    text = trim($code.text());
+    if (/^#!\/usr\/bin\/osascript\s/.test(text)) {
+      return $code.parent().addClass('brush:applescript').text(text);
+    } else {
+      return $code.addClass('prettyprint');
+    }
+  });
+  prettyPrint();
+  $time = $('time');
+  $comments = $time.filter(':has(span)');
+  $time.not($comments).localize();
+  $comments.localize({
+    escaped: true,
+    format: '<span>%d %mmmm %yyyy</span> <span>%h.%MM\u2009%a</span>',
+    periods: ['am', 'pm']
+  });
+  $('#nav').find('a[href="/flushcache/"]').click(function(event) {
+    get(this.href, function() {
+      return location.reload(true);
+    });
+    return event.preventDefault();
+  });
+  return $('textarea').each(function() {
+    return Hashify.editor(this);
+  });
 });
