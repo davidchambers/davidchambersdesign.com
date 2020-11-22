8859ed13213c6e30905d8971ae5fcea58a11000f
David Chambers <dc@davidchambers.me>
2020-11-23 00:26:36 +0100

    require array of children

--- templates/archives.clj
+++ templates/archives.clj
@@ -1,10 +1,23 @@
 (html {}
-  (head {}
-    (map (lambda [x]
-           (link {:rel "stylesheet"
-                  :href (str "https://cdn.jsdelivr.net/gh/davidchambers/davidchambersdesign.com@e76370b258761898ea0b63d1e14aa4caa4e7c359/src/css/"
-                             (get :name x))
-                  :media (get :media x)}))
-         [{:name "reset.css"  :media "all"}
-          {:name "print.css"  :media "print"}
-          {:name "screen.css" :media "screen"}])))
+  [(head {}
+     (map (lambda [x]
+            (link {:rel "stylesheet"
+                   :href (str "https://cdn.jsdelivr.net/gh/davidchambers/davidchambersdesign.com@e76370b258761898ea0b63d1e14aa4caa4e7c359/src/css/"
+                              (get :name x))
+                   :media (get :media x)}))
+          [{:name "reset.css"  :media "all"}
+           {:name "print.css"  :media "print"}
+           {:name "screen.css" :media "screen"}]))
+   (body {}
+     [(div {:id "wrap"}
+        [(div {:id "header"}
+           [(a {:id "title" :href "/TK"} ["David Chambers Design"])
+            (hr {})
+            (p {} ["It's where I share interesting info with other web geeks"])
+            (nav {:id "nav"}
+              [(ul {}
+                 [(li {} [(a {:href "/TK/about/"}     [(span {} [(strong {} ["About."])     " Who I am and what I do."])])])
+                  (li {} [(a {:href "/TK/contact/"}   [(span {} [(strong {} ["Contact."])   " Just in case you want to get in touch."])])])
+                  (li {} [(a {:href "/TK/archives/"}  [(span {} [(strong {} ["Archives."])  " Old posts, recent posts, they're all here."])])])
+                  (li {} [(a {:href "/TK/tags/"}      [(span {} [(strong {} ["Tags."])      " Helpful if you're after posts on a particular topic."])])])
+                  (li {} [(a {:href "/TK/bitbucket/"} [(span {} [(strong {} ["Bitbucket."]) " Who I am and what I do."])])])])])])])])])
--- templates/archives.env.js
+++ templates/archives.env.js
@@ -1,7 +1,19 @@
 import S from 'sanctuary';
 
+const transformAttrs = attrs => Object.fromEntries ((Object.getOwnPropertySymbols (attrs)).map (k => [(String (k)).slice ('Symbol('.length, -')'.length), attrs[k]]));
+
 export default {
-  html: (attrs, ...children) => ({tagName: 'html', attrs, children: S.Just (children.flat (1)), inline: false}),
-  head: (attrs, ...children) => ({tagName: 'head', attrs, children: S.Just (children.flat (1)), inline: false}),
-  link: (attrs) => ({tagName: 'link', attrs: Object.fromEntries ((Object.getOwnPropertySymbols (attrs)).map (k => [(String (k)).slice ('Symbol('.length, -')'.length), attrs[k]])), children: S.Nothing, inline: true}),
+  a: (attrs, children) => ({tagName: 'a', attrs: transformAttrs (attrs), children: S.Just (children.flat (1)), inline: true}),
+  body: (attrs, children) => ({tagName: 'body', attrs: transformAttrs (attrs), children: S.Just (children.flat (1)), inline: false}),
+  div: (attrs, children) => ({tagName: 'div', attrs: transformAttrs (attrs), children: S.Just (children.flat (1)), inline: false}),
+  head: (attrs, children) => ({tagName: 'head', attrs: transformAttrs (attrs), children: S.Just (children.flat (1)), inline: false}),
+  hr: (attrs, children) => ({tagName: 'hr', attrs: transformAttrs (attrs), children: S.Nothing, inline: true}),
+  html: (attrs, children) => ({tagName: 'html', attrs: transformAttrs (attrs), children: S.Just (children.flat (1)), inline: false}),
+  li: (attrs, children) => ({tagName: 'li', attrs: transformAttrs (attrs), children: S.Just (children.flat (1)), inline: true}),
+  link: (attrs) => ({tagName: 'link', attrs: transformAttrs (attrs), children: S.Nothing, inline: true}),
+  nav: (attrs, children) => ({tagName: 'nav', attrs: transformAttrs (attrs), children: S.Just (children.flat (1)), inline: false}),
+  p: (attrs, children) => ({tagName: 'p', attrs: transformAttrs (attrs), children: S.Just (children.flat (1)), inline: true}),
+  span: (attrs, children) => ({tagName: 'span', attrs: transformAttrs (attrs), children: S.Just (children.flat (1)), inline: true}),
+  strong: (attrs, children) => ({tagName: 'strong', attrs: transformAttrs (attrs), children: S.Just (children.flat (1)), inline: true}),
+  ul: (attrs, children) => ({tagName: 'ul', attrs: transformAttrs (attrs), children: S.Just (children.flat (1)), inline: false}),
 };
