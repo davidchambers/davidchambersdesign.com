a4b10ae70bb33bdc2e6ecbd2b06163aa416c6c25
David Chambers <dc@davidchambers.me>
2020-11-26 04:59:07 +0100

    use let

--- src/sanctuary.clj
+++ src/sanctuary.clj
@@ -1,14 +1,14 @@
 (import* ["./base.js"]
 
-(let' {:S (import "sanctuary")
-       :rename (lambda [name]
-                  (invoke "replace"
-                          [(new RegExp ["_" "g"])
-                           ""]
-                          (invoke "replace"
-                                  [(new RegExp ["(?!\\b)[A-Z]" "g"])
-                                   (lambda [c] (invoke "concat" [(invoke "toLowerCase" [] c)] "-"))]
-                                  name)))}
+(let [S (import "sanctuary")
+      rename (lambda [name]
+                (invoke "replace"
+                        [(new RegExp ["_" "g"])
+                         ""]
+                        (invoke "replace"
+                                [(new RegExp ["(?!\\b)[A-Z]" "g"])
+                                 (lambda [c] (invoke "concat" [(invoke "toLowerCase" [] c)] "-"))]
+                                name)))]
    (invoke "fromEntries"
            [(invoke "map"
                     [(lambda [kv] [(invoke "for" [(rename (0 kv))] Symbol) (1 kv)])]
