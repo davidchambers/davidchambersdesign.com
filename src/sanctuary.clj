c4a1483109091bde6c2d834b8700216e500fde55
David Chambers <dc@davidchambers.me>
2020-11-26 05:02:51 +0100

    inline

--- src/sanctuary.clj
+++ src/sanctuary.clj
@@ -1,15 +1,17 @@
 (import* ["./base.js"]
 
-(let [rename (lambda [name]
-                (invoke "replace"
-                        [(new RegExp ["_" "g"])
-                         ""]
-                        (invoke "replace"
-                                [(new RegExp ["(?!\\b)[A-Z]" "g"])
-                                 (lambda [c] (invoke "concat" [(invoke "toLowerCase" [] c)] "-"))]
-                                name)))]
-   (invoke "fromEntries"
-           [(invoke "map"
-                    [(lambda [kv] [(invoke "for" [(rename (0 kv))] Symbol) (1 kv)])]
-                    (invoke "entries" [(import "sanctuary")] Object))]
-           Object)))
+(invoke "fromEntries"
+        [(invoke "map"
+                 [(lambda [kv]
+                     [(invoke "for"
+                              [(invoke "replace"
+                                       [(new RegExp ["_" "g"])
+                                        ""]
+                                       (invoke "replace"
+                                               [(new RegExp ["(?!\\b)[A-Z]" "g"])
+                                                (lambda [c] (invoke "concat" [(invoke "toLowerCase" [] c)] "-"))]
+                                               (0 kv)))]
+                              Symbol)
+                      (1 kv)])]
+                 (invoke "entries" [(import "sanctuary")] Object))]
+        Object))
