c2c594e6c5935e6776475103724269fc53be6ff7
David Chambers <dc@davidchambers.me>
2022-03-27 14:07:08 +0200

    refactor render-archives.clj

--- src/site/render-archives.clj
+++ src/site/render-archives.clj
@@ -1,7 +1,21 @@
 (import* ["./elements"]
 
 (let [luxon (require "./luxon")
-      s (require "./sanctuary")]
+      s (require "./sanctuary")
+
+      render-section
+        (lambda [section]
+           (li
+              [(h2 (:heading section))
+               (ol
+                  (s/map (lambda [post]
+                            (li
+                               [(a' {:href (s/concat "/" (:slug post))} (:title post))
+                                " "
+                                (time {:datetime (luxon/to-iso (:datetime post))}
+                                   (s/concat (luxon/to-format "d MMMM y | h:mm" (:datetime post))
+                                             (s/to-lower (luxon/to-format "a" (:datetime post)))))]))
+                         (:posts section)))]))]
 
    (s/pipe [(s/map (s/join s/Pair))
             (s/map (s/map-left (s/prop :datetime)))
@@ -9,19 +23,6 @@
             (s/map (s/map-left (luxon/to-format "MMMM y")))
             (s/group-by (s/on s/equals s/fst))
             (s/chain (s/array [] (lambda [head tail] [{:heading (s/fst head) :posts (s/map s/snd (s/prepend head tail))}])))
-            (lambda [sections]
-               [(h1 "Archives")
-                (ol' {:class "archives"}
-                   (s/map (lambda [section]
-                             (li
-                                [(h2 (:heading section))
-                                 (ol
-                                    (s/map (lambda [post]
-                                              (li
-                                                 [(a' {:href (s/concat "/" (:slug post))} (:title post))
-                                                  " "
-                                                  (time {:datetime (luxon/to-iso (:datetime post))}
-                                                     (s/concat (luxon/to-format "d MMMM y | h:mm" (:datetime post))
-                                                               (s/to-lower (luxon/to-format "a" (:datetime post)))))]))
-                                           (:posts section)))]))
-                          sections))])])))
+            (s/map render-section)
+            (ol' {:class "archives"})
+            (s/concat [(h1 "Archives")])])))
